cd3226cd0550b628cde138dc04b90a7b2537d024
Dauphine Burns <dauphineburns@gmail.com>
2021-07-19 13:52:29 -0400

    main menu prompts

--- prompts.js
+++ prompts.js
@@ -1,28 +1,40 @@
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What would you like to do?',
-      name: '',
-    },
-    {
-      type: 'input',
-      message: '?',
-      name: '',
-    },
-    {
-      type: 'input',
-      message: '',
-      name: '',
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+
+function start () {
+    inquirer
+        .prompt({
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View all employees', 'View all employees by department', 'View all employees by manager', 'Add employee', 'Remove employee', 'Update employee role', 'Update employee manager', 'Exit'],
+        })
+        .then(function (response) {
+// Need 7 switch statements
+            switch (response.menu) {
+                case 'View all employees': allEmployees();
+                    break;
+                case 'View all employees by department': emplDept();
+                    break;
+                case 'View all employees by manager': emplMan();
+                    break;
+                case 'Add employee': addEmpl();
+                    break;
+                case 'Remove employee': removeEmpl();
+                    break;
+                case 'Update employee role': upEmplRole();
+                    break;
+                case 'Update employee manager': upEmplMan();
+                    break;
+                case 'Exit': exit();
+                    break;
+                    default;
+                    exit();
+            }
+        });
+};
+
+
+
 
 
 fs.writeFile(process.argv[2], `${response.username}: ${response.password}\n` , (err) => console.log(err));
